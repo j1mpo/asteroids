7d7474e168d9bd4bff8cfde727c128f71e9b5794
j1mpo <jim.polychronis@gmail.com>
2025-01-11 21:59:02 +0200

    Update gamestate.h

--- gamestate.h
+++ gamestate.h
@@ -2,8 +2,7 @@
 #include <string>
 #include <sgg/graphics.h>
 
-class GameState
-{
+class GameState {
 private:
     std::string m_asset_path = "assets\\";
 
@@ -11,16 +10,14 @@ private:
     float m_canvas_height = 6.0f;
 
     static GameState* m_unique_instance;
-
-    GameState();
-
     class Player* m_player = nullptr;
-    class Level* m_current_level = nullptr;
 
 public:
     float m_global_offset_x = 0.0f;
     float m_global_offset_y = 0.0f;
     bool m_debugging = false;
+
+    GameState();  // Προσθήκη του constructor για την GameState
 
     void init();
     void draw();
@@ -32,8 +29,10 @@ public:
     float getCanvasWidth() { return m_canvas_width; }
     float getCanvasHeight() { return m_canvas_height; }
 
-    std::string getAssetDir();
-    std::string getFullAssetPath(const std::string& asset);
+    std::string getAssetDir() { return m_asset_path; }
+    std::string getFullAssetPath(const std::string& asset) {
+        return m_asset_path + asset;
+    }
 
     class Player* getPlayer() { return m_player; }
 };
