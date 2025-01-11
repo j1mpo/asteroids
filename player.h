72e8b51406b0856c1721d531ffd593e3b10879f6
j1mpo <jim.polychronis@gmail.com>
2025-01-11 21:59:19 +0200

    Update player.h

--- player.h
+++ player.h
@@ -1,17 +1,20 @@
-// Player.h
-
 #pragma once
 #include "gameobject.h"
 #include "box.h"
 #include <sgg/graphics.h>
 
-class Player : public GameObject, public Box
-{
+class Player : public GameObject {
     graphics::Brush m_brush_player;
+    Box m_box;
 
 public:
-    // ???, ? ????????????? ????? ??? ???????????? ??? GameObject ?? ?? GameState*
-    Player(GameState* state, std::string name = "Player") : GameObject(state, name) {}
+    // Default Constructor
+    Player() : GameObject(nullptr, "Default"), m_box(0, 0, 1, 1) {}
+
+    // Constructor με παραμέτρους
+    Player(GameState* state, const std::string& name = "Player")
+        : GameObject(state, name), m_box(0, 0, 1, 1) {
+    }
 
     void update(float dt) override;
     void init() override;
