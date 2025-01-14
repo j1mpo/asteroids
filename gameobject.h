e168b5058305534410a84d8991fd9edb127a803c
j1mpo <jim.polychronis@gmail.com>
2025-01-14 18:26:08 +0200

    Update gameobject.h

--- gameobject.h
+++ gameobject.h
@@ -1,22 +1,34 @@
 #pragma once
-#include <string>
+//#include <string>
+#include "util.h"
 
-class GameState;
+class Collidable
+{
+public:
+    virtual Disk getCollisionHull() const = 0;
+    
+
+};
 
 class GameObject {
+
 protected:
-    float m_pos_x, m_pos_y;
-    GameState* m_state;
-    std::string m_name;
-    bool m_active = true;
+    const class Game& game;
+
+//protected:
+   // float m_pos_x, m_pos_y;
+   // GameState* m_state;
+   // std::string m_name;
+   // bool m_active = true;
 
 public:
-    GameObject(const std::string& name = "");  // Δήλωση χωρίς σώμα
-    virtual void update(float dt) = 0;
-    virtual void init() {}
-    virtual void draw() {}
-    bool isActive() { return m_active; }
-    void setActive(bool a) { m_active = a; }
 
-    virtual ~GameObject() {}
+    GameObject(const class Game & mygame); //std::string& name = "");  // ?????? ????? ????
+    virtual void update() = 0;
+    virtual void init() = 0;
+    virtual void draw() = 0;
+    //bool isActive() { return m_active; }
+    //void setActive(bool a) { m_active = a; }
+
+   // virtual ~GameObject() {}
 };
