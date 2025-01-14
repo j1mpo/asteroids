ec7ddae1ce3350779cfb556503061f10c2ed31cc
j1mpo <jim.polychronis@gmail.com>
2025-01-14 18:21:03 +0200

    Update gameobject.cpp

--- gameobject.cpp
+++ gameobject.cpp
@@ -1,9 +1,10 @@
 #include "gameobject.h"
-#include "gamestate.h"
+#include "game.h"
 
 
 
-GameObject::GameObject(GameState* state, const std::string& name)
-    : m_state(state), m_name(name), m_pos_x(0), m_pos_y(0) {
-   
+GameObject::GameObject(const Game & mygame)
+	:game(mygame)
+{
+
 }
