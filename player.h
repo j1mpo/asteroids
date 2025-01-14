e96044ae2f5070d5f169e476d1324d307d1ad3fa
j1mpo <jim.polychronis@gmail.com>
2025-01-14 18:29:25 +0200

    Update player.h

--- player.h
+++ player.h
@@ -1,25 +1,36 @@
 #pragma once
 #include "gameobject.h"
-#include "box.h"
-#include <sgg/graphics.h>
+#include "config.h"
 
-class Player : public GameObject {
+
+class Player : public GameObject, public Collidable
+{
    
-    //graphics::Brush m_brush_player;  // Brush για το κανονικό sprite
-    //graphics::Brush m_brush_glow;    // Brush για το glow effect
-    graphics::Brush m_brush_player;
-    Box m_box;
+    //graphics::Brush m_brush_player;  // Brush ??? ?? ???????? sprite
+    //graphics::Brush m_brush_glow;    // Brush ??? ?? glow effect
+    //graphics::Brush m_brush_player;
+    //Box m_box;
+    float speed = 2.0f;
+    float pos_x = CANVAS_WIDTH / 2, pos_y = CANVAS_HEIGHT / 2;
+    float height = 1.0f;
+    float life = 1.0f;
+
 
 public:
     // Default Constructor
     //Player() : GameObject("Default"), m_box(0, 0, 1, 1) {}
 
-    // Constructor με παραμέτρους
-    Player(std::string name) : GameObject(name), m_box(0, 0, 1, 1) {}
-
-    void update(float dt) override;
+    // Constructor ?? ???????????
+    Player(const class Game& mygame);
+    void update() override;
     void init() override;
     void draw() override;
+    float getPosY() { return pos_y; }
+    float getPosX() { return pos_x; }
+    Disk getCollisionHull() const override;
+    float getRemainingLife() const { return life;  }
+    void drainLife(float amount) { life = std::max(0.0f, life - amount); }
+
 
 
 
