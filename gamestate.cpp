fa36236a4e259044da2d9425cf86ce78828c97a0
j1mpo <jim.polychronis@gmail.com>
2025-01-14 03:59:06 +0200

    Update gamestate.cpp

--- gamestate.cpp
+++ gamestate.cpp
@@ -1,5 +1,6 @@
 #include "gamestate.h"
 #include "player.h"
+#include "level.h"
 
 GameState* GameState::m_unique_instance = nullptr;
 
@@ -8,27 +9,51 @@ GameState::GameState() {
 }
 
 void GameState::init() {
-    m_player = new Player();
+    m_player = new Player("Player1");
     m_player->init();
+
+    m_current_level = new Level();
+    m_current_level->init();
+
+
 }
 
 void GameState::draw() {
     // Σχεδίαση του παιχνιδιού
-    m_player->draw();
+    if (!m_current_level)
+        return;
+
+    m_current_level->draw();
 }
 
 void GameState::update(float dt) {
     // Ενημέρωση του παιχνιδιού
-    m_player->update(dt);
+    if (dt > 500)
+        return;
+
+
+    if (!m_current_level)
+        return;
+
+    m_current_level->update(dt);
 }
 
 GameState* GameState::getInstance() {
-    if (!m_unique_instance) {
+    if (m_unique_instance == nullptr) {
         m_unique_instance = new GameState();
     }
     return m_unique_instance;
 }
 
 GameState::~GameState() {
-    delete m_player;
+    if (m_player)
+        delete m_player;
+}
+
+std::string GameState::getAssetDir() {
+    return m_asset_path;
+}
+
+std::string GameState::getFullAssetPath(const std::string& asset) {
+    return m_asset_path + asset;
 }
