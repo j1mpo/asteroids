ba2df249b916dc42384727cf7fb9fe12f1731839
j1mpo <jim.polychronis@gmail.com>
2025-01-14 03:57:26 +0200

    Update level.h

--- level.h
+++ level.h
@@ -2,22 +2,23 @@
 #include "gameobject.h"
 #include "sgg/graphics.h"
 #include <list>
-#include <vector>
+#include "asteroid.h"
+#include <string>
 
-class Level : public GameObject
-{
-    graphics::Brush m_brush_background;
+class Level : public GameObject {
 
-    float m_center_x = 5.0f;
-    float m_center_y = 5.0f;
+	graphics::Brush m_brush_background;
+	std::vector<Asteroid*> m_asteroids;
+	float m_center_x = 0.5f;
+	float m_center_y = 0.5f;
 
-    std::vector<GameObject*> m_static_object;
-    std::list<GameObject*> m_dynamic_object;
+	std::vector<GameObject*> m_static_object;
+	std::list<GameObject*> m_dynamic_object;
 
 public:
-    void update(float dt) override;
-    void init() override;
-    void draw() override;
-    Level(const std::string& name = "Level0");
-    ~Level();
+	void update(float dt) override;
+	void init() override;
+	void draw() override;
+	Level(const std::string& name = "level0");
+	~Level();
 };
