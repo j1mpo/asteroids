4730793e20197949f0630d68284e9d2103c8fc72
j1mpo <jim.polychronis@gmail.com>
2025-01-14 12:59:16 +0200

    Update player.h

--- player.h
+++ player.h
@@ -4,19 +4,23 @@
 #include <sgg/graphics.h>
 
 class Player : public GameObject {
+   
+    //graphics::Brush m_brush_player;  // Brush για το κανονικό sprite
+    //graphics::Brush m_brush_glow;    // Brush για το glow effect
     graphics::Brush m_brush_player;
     Box m_box;
 
 public:
     // Default Constructor
-    Player() : GameObject(nullptr, "Default"), m_box(0, 0, 1, 1) {}
+    //Player() : GameObject("Default"), m_box(0, 0, 1, 1) {}
 
     // Constructor με παραμέτρους
-    Player(GameState* state, const std::string& name = "Player")
-        : GameObject(state, name), m_box(0, 0, 1, 1) {
-    }
+    Player(std::string name) : GameObject(name), m_box(0, 0, 1, 1) {}
 
     void update(float dt) override;
     void init() override;
     void draw() override;
+
+
+
 };
