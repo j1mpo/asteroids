40e4f72d89005d998cf75dd7c24e453d92fa53c6
j1mpo <jim.polychronis@gmail.com>
2025-05-08 21:46:54 +0300

    Update gameobject.h

--- gameobject.h
+++ gameobject.h
@@ -14,21 +14,16 @@ class GameObject {
 
 protected:
     const class Game& game;
-
-    //protected:
-       // float m_pos_x, m_pos_y;
-       // GameState* m_state;
-       // std::string m_name;
-       // bool m_active = true;
+    bool m_active = true;
 
 public:
 
-    GameObject(const class Game& mygame); //std::string& name = "");  // ?????? ????? ????
+    GameObject(const class Game& mygame);
     virtual void update() = 0;
     virtual void init() = 0;
     virtual void draw() = 0;
-    //bool isActive() { return m_active; }
-    //void setActive(bool a) { m_active = a; }
+    bool isActive() { return m_active; }
+    void setActive(bool a) { m_active = a; }
 
-   // virtual ~GameObject() {}
+    ~GameObject() {}
 };
