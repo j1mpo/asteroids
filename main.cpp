6e464fa5561d2d97655e5ccfca4be499d367a8bb
j1mpo <jim.polychronis@gmail.com>
2025-05-08 21:47:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <sgg/graphics.h>
 #include "config.h"
 
-
+//main 
 
 void resize(int w, int h)
 {
@@ -12,7 +12,7 @@ void resize(int w, int h)
 }
 
 void draw() {
-    Game* game = reinterpret_cast<Game *>(graphics::getUserData());
+    Game* game = reinterpret_cast<Game*>(graphics::getUserData());
     game->draw();
 }
 
@@ -20,21 +20,22 @@ void update(float ms) {
     Game* game = reinterpret_cast<Game*>(graphics::getUserData());
     game->update();
 }
-
+//main methodos edw trexei to paixnidi
 int main() {
 
     Game mygame;
 
-    graphics::createWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Spaceship Game");
+    graphics::createWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Krabby Patties");
     graphics::setUserData(&mygame);
     graphics::setDrawFunction(draw);
     graphics::setUpdateFunction(update);
     graphics::setCanvasSize(WINDOW_WIDTH, WINDOW_HEIGHT);
     graphics::setCanvasScaleMode(graphics::CANVAS_SCALE_FIT);
+    graphics::setResizeFunction(resize);
 
 
     mygame.init();
-    mygame.setDebugModel(true);
+    mygame.setDebugModel(false); //false svinei hitbox kai cords voitha sto development
     graphics::startMessageLoop();
 
     return 0;
