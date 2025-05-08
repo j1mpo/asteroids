cee4e3cf7fd34cbed2a9e16a88a238f0547e603e
j1mpo <jim.polychronis@gmail.com>
2025-05-08 21:47:53 +0300

    Update player.h

--- player.h
+++ player.h
@@ -1,26 +1,20 @@
 #pragma once
 #include "gameobject.h"
 #include "config.h"
+#include "util.h"+
 
 
 class Player : public GameObject, public Collidable
 {
-   
-    //graphics::Brush m_brush_player;  // Brush ??? ?? ???????? sprite
-    //graphics::Brush m_brush_glow;    // Brush ??? ?? glow effect
-    //graphics::Brush m_brush_player;
-    //Box m_box;
+
     float speed = 2.0f;
-    float pos_x = CANVAS_WIDTH / 2, pos_y = CANVAS_HEIGHT / 2;
+    float pos_x = CANVAS_WIDTH / 2, pos_y = CANVAS_HEIGHT - 50.0f; //topothetish xarakthra sto kentro
     float height = 1.0f;
     float life = 1.0f;
 
 
 public:
-    // Default Constructor
-    //Player() : GameObject("Default"), m_box(0, 0, 1, 1) {}
 
-    // Constructor ?? ???????????
     Player(const class Game& mygame);
     void update() override;
     void init() override;
@@ -28,10 +22,9 @@ public:
     float getPosY() { return pos_y; }
     float getPosX() { return pos_x; }
     Disk getCollisionHull() const override;
-    float getRemainingLife() const { return life;  }
+    float getRemainingLife() const { return life; }
     void drainLife(float amount) { life = std::max(0.0f, life - amount); }
-
-
+    void gainLife(float amount);
 
 
 };
